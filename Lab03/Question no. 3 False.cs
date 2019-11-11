95c69207400b51431dce38570abf67e04e6594af
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-11 15:43:10 -0500

    Add files via upload

---
+++ Lab03/Question no. 3 False.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace False
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isTrue = 20;
+
+            if (isTrue <= 10)
+                Console.WriteLine("It is True");
+            else
+                Console.WriteLine("It is False");
+
+
+         
+
+
+
+
+        }
+    }
+}
---
+++ Lab03/Question no. 3 True.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isTrue = 20;
+            if (isTrue <= 10)
+                Console.WriteLine("It is True");
+            else
+                Console.WriteLine("It is False");
+        }
+    }
+}
