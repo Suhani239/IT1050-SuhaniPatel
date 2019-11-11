9bf68f5670589ee6c33f77081ffd9f0f4216a927
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-11 15:33:15 -0500

    Add files via upload

---
+++ Lab03/Question no. 6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 60;
+            while (a >= 20)
+            {
+                Console.WriteLine(a);
+                a--;
+            }
+
+
+        }
+    }
+}
