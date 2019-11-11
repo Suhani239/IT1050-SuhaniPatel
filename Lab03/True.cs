f5e30d98494604d948c998a85bf165ec7a87e7c0
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-11 13:33:48 -0500

    Add files via upload

---
+++ Lab03/True.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isTrue = 10;
+            if (isTrue <= 60)
+                Console.WriteLine("It is True");
+            else
+                Console.WriteLine("It is False");
+        }
+    }
+}
