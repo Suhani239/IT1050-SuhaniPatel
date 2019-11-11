ef3bd63f60f4067145306f337ce3413017af6cc8
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-11 12:52:26 -0500

    Add files via upload

---
+++ Lab03/Question no. 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speed;
+            int speedLimit;
+
+            speedLimit = 35;
+            speed = 42;
+
+            if (speed > speedLimit)
+            {
+                Console.WriteLine("SLOW NOW");
+            }
+
+
+
+        }
+    }
+}
