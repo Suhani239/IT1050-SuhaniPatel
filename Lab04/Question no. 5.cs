b0f39dd172802c91e001207ce043becbb935990f
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-17 21:05:31 -0500

    Add files via upload

---
+++ Lab04/Question no. 5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Question_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            Console.WriteLine("Please enter a temperature");
+            int temperature = Convert.ToInt32(Console.ReadLine());
+
+            {     
+                if (temperature < 10)
+                    Console.WriteLine("Polar bear");
+                else if (temperature < 20)
+                    Console.WriteLine("Penguin");
+                else if (temperature < 40)
+                    Console.WriteLine("Moose");
+                else if (temperature < 50)
+                    Console.WriteLine("Reindeer");
+                else if (temperature < 60)
+                    Console.WriteLine("Deer");
+                else if (temperature < 70)
+                    Console.WriteLine("Turtle");
+                else if (temperature < 80)
+                    Console.WriteLine("Lion");
+                else if (temperature < 90)
+                    Console.WriteLine("Fish");
+            }
+        }
+
+       
+
+            
+        
+    }
+}
