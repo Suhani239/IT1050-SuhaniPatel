94beca6b03fe5396f5d2570ad8509b43fcc7d08f
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-18 15:31:01 -0500

    Add files via upload

---
+++ Lab04/Question no. 4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+                {
+                    if ((i % 2) == 0)
+                    {
+                        Console.WriteLine("Number is even");
+                    }
+                    else if ((i % 2) != 0)
+                    {
+                        Console.WriteLine("Number is odd");
+                    }
+
+                }
+            }
+        }
+    }
+}
