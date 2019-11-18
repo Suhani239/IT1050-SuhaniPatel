ea1d97169a89a9ed449446f391b7d9502d17a24b
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-17 21:46:01 -0500

    Add files via upload

---
+++ Lab04/Question no. 7 .cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Question_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            while (i < 21)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
