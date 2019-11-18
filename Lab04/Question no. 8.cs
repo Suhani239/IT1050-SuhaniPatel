f07e796ac0c1faa4377fafe1a7b7189c1251e522
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-18 15:48:44 -0500

    Add files via upload

---
+++ Lab04/Question no. 8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Question_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("********");
+            }
+        }
+    }
+}
