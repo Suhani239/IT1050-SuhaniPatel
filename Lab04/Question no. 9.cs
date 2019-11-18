89afd3aaba5e9e1155cb091bc14a08a031c29670
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-18 16:28:52 -0500

    Add files via upload

---
+++ Lab04/Question no. 9.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Question_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   //1st Triangle (A)
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            //2nd Triangle (B)
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+            //3rd Triangle (C)
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            //4th Triangle (D)
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
