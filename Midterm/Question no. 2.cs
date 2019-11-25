3decdba6f0d8e7777e0784c6eafcddc5774633b0
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-25 15:58:41 -0500

    Add files via upload

---
+++ Midterm/Question no. 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Q_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            Boolean keepLooping = true;
+            while (keepLooping)
+            {
+                if (i <= 19)
+
+                    keepLooping = false;
+                i++;
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
