c6783519feff5163220ddfc19619b2d74eef1bb6
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-25 17:30:21 -0500

    Add files via upload

---
+++ Midterm/Question no. 5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Q._no_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <=21)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.Write(" ");
+                }
+                else if ((i % 2) != 0)
+                {
+                    Console.Write(i);
+                }
+                i++;
+            }
+                
+        }
+    }
+}
