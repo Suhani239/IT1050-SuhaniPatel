61ffbee91c0b914d64f1ae4f52a7c6cff7fbd0ff
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-25 17:46:23 -0500

    Add files via upload

---
+++ Midterm/Question no. 7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Q_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool icyrain = false, tornadowarning = false;
+            if(icyrain == false && tornadowarning == false)
+            {
+                Console.WriteLine("Lets go outside");
+            }
+        }
+    }
+}
