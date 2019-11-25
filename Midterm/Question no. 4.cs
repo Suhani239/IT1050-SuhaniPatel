82f711534d902f55160684f4ab639db745878da9
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-25 16:40:47 -0500

    Add files via upload

---
+++ Midterm/Question no. 4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Q_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string j = ",";
+            int i;
+            for (i= 49; i >= 1; --i)
+            {
+                Console.Write(i);
+                if (i>=2)
+                {
+                    Console.Write(j);
+                }
+            }
+        }
+    }
+}
