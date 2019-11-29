e88db966225670e3db49fc3d9fd75b2681f8c874
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-11-29 01:41:25 -0500

    Add files via upload

---
+++ Lab05/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+
+        private void bark()
+        {
+            Console.WriteLine("{0} is Barking....", Name);
+        }
+
+        private void doTrick(string trickName)
+        {
+            Console.WriteLine("{0} is so smart! {0} is doing a(n) {1}", Name, trickName);
+        }
+    }
+}
