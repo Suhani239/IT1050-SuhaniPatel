1f2cf772a6994434d17f152579cac7ade10bccc9
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-12-02 13:46:15 -0500

    Add files via upload

---
+++ Lab06/Question no. 4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNumber = new int[1000];
+            Random random = new Random();
+
+            for (int i = 0; i < randomNumber.Length; i++)
+            {
+                randomNumber[i] = random.Next(0, 100);
+            }
+
+            foreach (int i in randomNumber)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+
+
+        }
+    }
+}
