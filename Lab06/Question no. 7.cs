b5bafa7f6bc36b60e2fd0128b2f7d1241bb9b57d
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-12-02 14:15:49 -0500

    Add files via upload

---
+++ Lab06/Question no. 7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Q_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+            int i = 0;
+            foreach (string name in names)
+            {
+                Console.WriteLine("{0,2}. {1}", i, names[i]);
+                i++;
+            }
+        }
+    }
+}
