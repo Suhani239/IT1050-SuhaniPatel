994a66604b38aae5854c797d59c771fe37fce241
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-12-02 14:46:22 -0500

    Add files via upload

---
+++ Lab06/Question no. 3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Q_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seasons = { "Spring", "Summer", "Fall", "Winter" };
+            
+            foreach (string season in seasons)
+            {
+                Console.WriteLine(season);
+            }
+        }
+    }
+}
