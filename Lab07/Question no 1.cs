c9456db8e13f1b15e32cb4745aa6cf75cad518e1
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-12-07 15:49:12 -0500

    Add files via upload

---
+++ Lab07/Question no 1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7
+{
+    class Book
+    {
+        private string Title;
+        private string Author;
+        private int Year;
+
+        public Book(string Title, String Author, int Year)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Year = Year;
+        }
+        public Book(string Title, string Author)
+        {
+            this.Title = Title;
+            this.Author = Author;
+        }
+          
+        public void Display()
+        {
+            Console.WriteLine(" The title is {0} and the author is {1} ", Title, Author);
+        }
+           
+            
+        
+
+
+
+
+
+    }
+}
