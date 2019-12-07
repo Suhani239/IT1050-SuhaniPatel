1781f83778befe239518942bbc855306c9f0d8df
Suhani239 <54646346+Suhani239@users.noreply.github.com>
2019-12-07 16:16:06 -0500

    Add files via upload

---
+++ Lab07/Question no. 2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class BookTest
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book("Macbeth", "William Shakespeare", 1623);
+
+            myBook.Display();
+        }
+    }
+}
